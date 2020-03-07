8e4a0e08feaf3f843982c9d6976c0ad52cc5f342
JaniceShah <45307828+JaniceShah@users.noreply.github.com>
2020-03-08 03:45:10 +0530

    Update cam.page.ts

--- src/app/pages/cam/cam.page.ts
+++ src/app/pages/cam/cam.page.ts
@@ -13,18 +13,25 @@ const MEDIA_FILES_KEY = 'mediaFiles';
   styleUrls: ['./cam.page.scss'],
 })
 export class CamPage implements OnInit {
-  mediaFiles = [];
+mediaFiles = [];
+  captureVideoUrl : String;
   @ViewChild('myvideo',{static:false}) myVideo: any;
-  constructor(public navCtrl: NavController, private mediaCapture: MediaCapture, private file: File, private media: Media) { }
+  constructor(public navCtrl: NavController, private mediaCapture: MediaCapture, private db: AngularFireDatabase, private firebaseApp: AngularFireStorage,private afStorage: Storage, private file: File, private media: Media) { }
 
   ngOnInit() {
   }
 
-
-  captureAudio() {
-    this.mediaCapture.captureAudio().then(res => {
-    });
+  ionViewDidLoad() {
+    this.afStorage.get(MEDIA_FILES_KEY).then(res => {
+      this.mediaFiles = JSON.parse(res) || [];
+    })
   }
+
+   captureAudio() {
+     this.mediaCapture.captureAudio().then(res => {
+   //    this.storeMediaFiles(res);
+     });
+   }
 
   captureVideo() {
     let options: CaptureVideoOptions = {
@@ -32,22 +39,34 @@ export class CamPage implements OnInit {
       duration: 10
     }
   this.mediaCapture.captureVideo(options).then((res: MediaFile[]) => {
-    let capturedFile = res[0];
-    let fileName = capturedFile.name;
-    let dir = capturedFile['localURL'].split('/');
-    dir.pop();
-    let fromDirectory = dir.join('/');      
-    var toDirectory = this.file.dataDirectory;
+    let fpath = "D:\\Dev\\safeher\\SafeHer";
+    let name = 'saveme';
+    let path = fpath.replace(name, '');
     
-    this.file.copyFile(fromDirectory , fileName , toDirectory , fileName).then((res) => {
-    },err => {
-      console.log('err: ', err);
+    this.file.readAsDataURL(path, name).then(dataText=>{
+   //set the var
+      this.captureVideoUrl = dataText;
+      let uploadtask = this.uploadVideo('Victor456');
+        uploadtask.on('state_changed', (snapshot) => {
+
+        }, (error: Error) =>{
+      }, () =>{
+          let url = uploadtask.snapshot.downloadURL;          
+      })
+    }).catch(err =>{
     });
-        },
-  (err: CaptureError) => console.error(err));
+  }).catch(err=>{
+  })
 }
 
-play(myFile) {
+uploadVideo(userId: string): firebase.storage.UploadTask{
+  
+  // Create a timestamp as filename
+  const filename = userId;
+  return this.afStorage.ref(`videos/${filename}.mp4`).putString(this.captureVideoUrl, firebase.storage.StringFormat.DATA_URL);
+}
+
+  play(myFile) {
   if (myFile.name.indexOf('.wav') > -1) {
     const audioFile: MediaObject = this.media.create(myFile.localURL);
     audioFile.play();
